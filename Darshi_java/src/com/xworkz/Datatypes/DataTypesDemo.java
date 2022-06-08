b5669324aa7aa6c4c699637a12fcf1ce8816d6cd
dharshanchannapatna <dharshanv.xworkz@gmail.com>
2022-06-08 15:36:32 +0530

    you

---
+++ Darshi_java/src/com/xworkz/Datatypes/DataTypesDemo.java
@@ -0,0 +1,26 @@
+package com.xworkz.Datatypes;
+
+public class DataTypesDemo {
+int y=435678;
+static short z=32767;
+long L=1257896488563l;
+	public static void main(String[] args) {
+		DataTypesDemo s=new DataTypesDemo();
+		byte x=127;
+		float a=10.7f;
+		double b=5.7d;
+		System.out.println(x);
+		System.out.println(s.y);
+		System.out.println(z);
+		System.out.println(s.L);
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
+/* output x=127
+y=435678
+z=32767
+L=125896488563
+a=10.7
+b=5.7 */
