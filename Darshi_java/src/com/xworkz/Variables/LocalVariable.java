2ef9391c783a28d15367ee42935a69b063766351
dharshanchannapatna <dharshanv.xworkz@gmail.com>
2022-06-08 15:01:15 +0530

    you

---
+++ Darshi_java/src/com/xworkz/Variables/LocalVariable.java
@@ -0,0 +1,11 @@
+package com.xworkz.Variables;
+
+public class LocalVariable {
+
+	public static void main(String[] args) {
+		int x=10;
+		System.out.println(x);
+
+	}
+
+}
---
+++ Darshi_java/src/com/xworkz/Variables/StaticVariable.java
@@ -0,0 +1,12 @@
+package com.xworkz.Variables;
+
+public class StaticVariable {
+static int x=10;
+	public static void main(String[] args) {
+		 StaticVariable t=new  StaticVariable();
+		System.out.println(StaticVariable.x);
+		System.out.println(x);
+		System.out.println(t.x);
+	}
+
+}
