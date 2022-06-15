2cb74f74b7d549b2d9b6869d8c5bac09450f13d3
dharshanchannapatna <dharshanv.xworkz@gmail.com>
2022-06-15 21:19:02 +0530

    primitivetypecasting

---
+++ Darshi_java/src/com/xworkz/Typecasting/Primitivetypecastingassignment/primitivetypecasting.java
@@ -0,0 +1,12 @@
+package com.xworkz.Typecasting.Primitivetypecastingassignment;
+
+public class primitivetypecasting {
+
+	public static void main(String[] args) {
+       int x='a';
+       System.out.println(x);
+       
+
+	}
+
+}
