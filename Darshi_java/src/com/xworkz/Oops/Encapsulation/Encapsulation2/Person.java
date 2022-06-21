82e87dbd06b5831386b98f4b43d2a53f43b338a2
dharshanchannapatna <dharshanv.xworkz@gmail.com>
2022-06-21 23:53:16 +0530

    Encapsulation

---
+++ Darshi_java/src/com/xworkz/Oops/Encapsulation/Encapsulation2/Person.java
@@ -0,0 +1,13 @@
+package com.xworkz.Oops.Encapsulation.Encapsulation2;
+public class Person {
+	private int age;
+	
+	public void SetAge(int age) {
+		this.age=age;
+		
+	}
+    public int Getage() {
+	return age; 
+}
+	
+}
---
+++ Darshi_java/src/com/xworkz/Oops/Encapsulation/Encapsulation2/TestPerson.java
@@ -0,0 +1,20 @@
+package com.xworkz.Oops.Encapsulation.Encapsulation2;
+
+public class TestPerson {
+
+	public static void main(String[] args) {
+	       Person p=new Person();
+	       p.SetAge(25);
+	       System.out.println(p.Getage());
+	       
+	      
+	       
+		
+	   
+	       
+	    
+	      
+
+	}
+
+}
